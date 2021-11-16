90c9abafe95eabb3f17847e6fb7f5a7d423dbb91
Antonio Trigo <antonio.trigo@gmail.com>
2021-11-16 14:25:52 +0000

    Create exercicio8.27.cs

---
+++ Capitulo 8/Exercicios/exercicio8.27.cs
@@ -0,0 +1,44 @@
+/*
+ * Exercício 8.27
+ * Livro: Aprenda a programar com C#
+ * Autores: António Trigo e Jorge Henriques
+ * Disponível em: http://www.silabo.pt
+*/
+using System;
+
+namespace Cap8
+{
+    class Program
+    {
+		static void ApresentaVetor(int[] v){
+			for (int i = 0; i < v.Length; i++)
+                Console.Write("{0} " , v[i]);
+			Console.WriteLine();
+		}
+        
+		static void InsertionSort(int[] v)
+        {
+            for (int i = 0; i < v.Length - 1; i++)
+            {
+                int min_pos = i;
+                for (int j = i + 1; j < v.Length; j++)
+                    if (v[j] < v[min_pos])
+                        min_pos = j;
+     
+                int temp = v[min_pos];
+                v[min_pos] = v[i];
+                v[i] = temp;
+            }
+        }
+
+        public static void Main()
+        {
+            int[] v = {3,4,6,2,3,9,1,2,9,1};
+            Console.WriteLine("Vetor original: ");
+			ApresentaVetor(v);
+			InsertionSort(v);
+            Console.WriteLine("Vetor ordenado: ");
+			ApresentaVetor(v);
+        }
+    }
+}
